6f221b6220fc1469e5002047c0c3e337efd0de88
athar <ibnulivo1903@gmail.com>
2024-08-22 09:05:03 +0700

    belajar datatype

---
+++ datatype.js
@@ -0,0 +1,83 @@
+// 1.priimitvive
+// a --> string 
+// 2.complex
+
+// Numbers:
+// let length = 16;
+// let weight = 7.5;
+
+// // Strings:
+// let color = "Yellow";
+// let lastName = "Johnson";
+
+// // Booleans
+// let x = true;
+// let y = false;  justify content sapce around
+
+
+
+
+
+let word = 214;
+console.log('${word}')
+
+
+/*
+konversi tipe data
+1.string
+2.number
+3.bolean
+    a.ada teks nya
+    b.nilai nya bukan 0
+    c.true
+4.bigInt
+*/
+
+let number1 = 30;
+let number2 = 50;
+console.log(number1 + number2);
+
+
+const word1 = "abcde"
+console.log(word1.length)
+
+
+/**
+ * complex data type
+    1.object ---> ananlogi seperti benda
+        punya properti
+ */
+
+    // let object1 = {}
+    // console.log(typeof object1)
+
+
+    // let manusia1 = {
+    //     nama: "adam"
+    //     alamat: "jakarta"
+    //     umur: "54"
+    // };
+
+    // console.log(manusia1["nama"], ["alamat"])
+
+    //contoh array
+
+    // let array1 = [];
+    // console.log(array1); 
+    // console.log(array1 instanceof array1);
+
+
+    //studi kasus
+
+    let array = ["abc",123,true,null];
+    console.log(array.slice[0,3])
+
+    let user4 = [
+        {nama: "adam", addreas: "bekasi"}
+    ]
+    console.log(user4[0])
+
+
+    let mobil = [warna = "hijau", tipe = "volkwagen", ];
+    mobil.roda = "bulat";
+    console.log(mobil[1])
